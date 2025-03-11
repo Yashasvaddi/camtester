aa3851deb5a160ec08c3d37d34f785ca9f5fdf45
Yashasvaddi <yashas.vaddi12@gmail.com>
2025-03-11 17:45:33 +0530

    test file added

---
+++ camdisplay.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import cv2
+
+cap=cv2.VideoCapture(0)
+placeholder=st.empty()
+while(True):
+    ret,frame=cap.read()
+    frame=cv2.flip(frame,1)
+    placeholder.image(frame,channels="BGR")
+    # cv2.imshow("frame",frame)
+    if(cv2.waitKey(1)==ord('x')):
+        break
